c3ec82c4ab1811cc5803862b756545f37edf2c48
Paulin-Zumstein <60385111+Paulin-Zumstein@users.noreply.github.com>
2022-07-30 17:30:09 +0200

    add publishBook funtion

--- src/main/java/cvut/fel/service/PublishingHouseServiceImp.java
+++ src/main/java/cvut/fel/service/PublishingHouseServiceImp.java
@@ -1,11 +1,15 @@
 package cvut.fel.service;
 
 import cvut.fel.entity.Author;
+import cvut.fel.entity.Book;
+import cvut.fel.entity.Library;
 import cvut.fel.entity.PublishingHouse;
 import cvut.fel.exception.FieldMissingException;
 import cvut.fel.exception.NotFoundException;
 import cvut.fel.repository.AuthorRepository;
+import cvut.fel.repository.LibraryRepository;
 import cvut.fel.repository.PublishingHouseRepository;
+import cvut.fel.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +17,15 @@ import org.springframework.stereotype.Service;
 public class PublishingHouseServiceImp implements PublishingHouseService{
     private final PublishingHouseRepository publishingHouseRepository;
     private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final LibraryRepository libraryRepository;
 
     @Autowired
-    public PublishingHouseServiceImp(PublishingHouseRepository publishingHouseRepository, AuthorRepository authorRepository) {
+    public PublishingHouseServiceImp(PublishingHouseRepository publishingHouseRepository, AuthorRepository authorRepository, BookRepository bookRepository, LibraryRepository libraryRepository) {
         this.publishingHouseRepository = publishingHouseRepository;
         this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.libraryRepository = libraryRepository;
     }
 
     public PublishingHouse findById(Long id){
@@ -29,6 +37,10 @@ public class PublishingHouseServiceImp implements PublishingHouseService{
 
     public boolean checkAuthor(PublishingHouse publishingHouse, Author author){
         return !publishingHouse.getAuthors().contains(author);
+    }
+
+    public boolean checkBookNotPublished(PublishingHouse publishingHouse, Book book){
+        return !publishingHouse.getPublished_books().contains(book);
     }
 
     public PublishingHouse createContract(Long idPublishingHouse, Long idAuthor){
@@ -46,6 +58,22 @@ public class PublishingHouseServiceImp implements PublishingHouseService{
             return publishingHouse;
         }else{
             throw  new NotFoundException("AUTHOR_ALREADY_IN_PUBLISHING_HOUSE");
+        }
+
+    }
+
+    public void publishNewBook(Long idBook, Long idPublishingHouse){
+        if (idBook == null || idPublishingHouse == null)
+            throw new FieldMissingException();
+
+        PublishingHouse publishingHouse = publishingHouseRepository.findById(idPublishingHouse).get();
+        Book book = bookRepository.findById(idBook).get();
+
+        if(checkBookNotPublished(publishingHouse, book)){
+            publishingHouse.addPublishedBook(book);
+            publishingHouseRepository.save(publishingHouse);
+        }else{
+            throw  new NotFoundException("BOOK_ALREADY_PUBLISHED");
         }
 
     }
